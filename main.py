a8a3538892ce8d6c63e624fc321f25f7519ed1e8
LBJ-Nor <80926104+LBJ-Nor@users.noreply.github.com>
2021-07-16 02:01:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 # Title: Tic-Tac-Toe/Connect Three
-# Version: beta
 # To-Do: Add win_check
+
 
 import tkinter as tk
 
 move_counter = 0
+win_counter_X = 0
+win_counter_O = 0
+
 window = tk.Tk()
 window.title('Tic-Tac-Toe')
+window.geometry('450x600')
+window.config(bg='#222222')
 
 
 # return player_piece as string.
@@ -27,14 +32,24 @@ def move_validator(button_pressed):
         print('Invalid move!')
 
 
+# Button frame
+button_frame = tk.Frame(bg='#222222')
+button_frame.pack(side=tk.TOP)
+
+# Game text label
+game_text = tk.Label(bg='#a39c87', width=40, height=10, )
+game_text.pack(side=tk.LEFT)
+
 # Create buttons
 buttons_list = []
 for button in range(9):
-    btn = tk.Button(window, text='',
-                    borderwidth=1,
-                    height=6,
-                    width=18,
-                    bg='#777777'
+    btn = tk.Button(button_frame, text='',
+                    height=2,
+                    width=6,
+                    bg='#777777',
+                    font=('calibri', 30),
+                    borderwidth=8,
+                    activebackground='orange4'
                     )
     btn.config(command=lambda button=button: buttons_list[button].config(text=move_validator(buttons_list[button])))
     buttons_list.append(btn)
